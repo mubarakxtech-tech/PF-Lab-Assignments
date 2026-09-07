aa97e773027a78212b90797d111545c05c6c9342
mubarakxtech-tech <mubarakxtech@gmail.com>
2026-09-07 12:34:07 +0500

    add assing_2.c to lab_num folder

---
+++ assing_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int num1, num2;
+    printf("\nenter  number1:\t");
+    scanf("%d", &num1);
+    printf("\nenter  number2:\t");
+    scanf("%d", &num2);
+    printf("\nthe number is: %d\n", num1);
+    printf("\nthe number is: %d\n", num2);
+    return 0;
+}
